26c94f4aaea09d817f598db73768b9d838d4c077
Eduardo Gomez <this.eduardo@gmail.com>
2024-11-02 19:38:11 +0100

    Moved types to their own file

--- src/view-products.tsx
+++ src/view-products.tsx
@@ -1,65 +1,11 @@
 import { getPreferenceValues, List, ActionPanel, Action } from "@raycast/api";
 import { useFetch } from "@raycast/utils";
+import { ProductsResponse } from "./types";
 
 const token = getPreferenceValues<Preferences>().token;
 
-type Response = {
-  success: boolean;
-  products: Product[];
-};
-
-type Product = {
-  custom_permalink: string | null;
-  custom_receipt: string | null;
-  custom_summary: string;
-  custom_fields: any[];
-  customizable_price: number | null;
-  description: string;
-  deleted: boolean;
-  max_purchase_count: number | null;
-  name: string;
-  preview_url: string | null;
-  require_shipping: boolean;
-  subscription_duration: string | null;
-  published: boolean;
-  url: string;
-  id: string;
-  price: number;
-  purchasing_power_parity_prices: Record<string, number>;
-  currency: string;
-  short_url: string;
-  thumbnail_url: string;
-  tags: string[];
-  formatted_price: string;
-  file_info: Record<string, any>;
-  sales_count: string;
-  sales_usd_cents: string;
-  is_tiered_membership: boolean;
-  recurrences: string[] | null;
-  variants: Variant[];
-};
-
-type Variant = {
-  title: string;
-  options: Option[];
-};
-
-type Option = {
-  name: string;
-  price_difference: number;
-  purchasing_power_parity_prices: Record<string, number>;
-  is_pay_what_you_want: boolean;
-  recurrence_prices: Record<string, RecurrencePrice> | null;
-};
-
-type RecurrencePrice = {
-  price_cents: number;
-  suggested_price_cents: number | null;
-  purchasing_power_parity_prices: Record<string, number>;
-};
-
 export default function Command() {
-  const { data } = useFetch<Response>(
+  const { data } = useFetch<ProductsResponse>(
     `https://api.gumroad.com/v2/products?access_token=${token}`
   );
 
